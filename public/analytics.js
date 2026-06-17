e34676ed3af9a54136e86bc7e0556f8e4be56f08
RickyLei7 <Rickylei7@gmail.com>
2026-06-16 21:55:32 -0600

    Track social and AYCE clicks

--- public/analytics.js
+++ public/analytics.js
@@ -13,6 +13,7 @@
     if (!link) return;
 
     var href = link.getAttribute("href") || "";
+    var text = (link.textContent || "").trim().toLowerCase();
 
     if (href.indexOf("tel:") === 0) {
       sendEvent("phone_click", link);
@@ -20,6 +21,16 @@
       sendEvent("email_click", link);
     } else if (href.indexOf("google.com/maps") !== -1) {
       sendEvent("directions_click", link);
+    } else if (href.indexOf("instagram.com") !== -1) {
+      sendEvent("instagram_click", link);
+    } else if (href.indexOf("facebook.com") !== -1) {
+      sendEvent("facebook_click", link);
+    } else if (href.indexOf("xiaohongshu.com") !== -1) {
+      sendEvent("xiaohongshu_click", link);
+    } else if (href === "#ayce-poster") {
+      sendEvent("ayce_poster_click", link);
+    } else if (href === "#specials" || text.indexOf("ayce") !== -1) {
+      sendEvent("ayce_interest_click", link);
     } else if (href.indexOf("/menu") === 0 || href.indexOf("menu/") === 0) {
       sendEvent("menu_click", link);
     }
